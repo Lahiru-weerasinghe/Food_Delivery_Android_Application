424ff9717f21caf8d60fbd34fc858c2620a01c1b
Dilshan Perera <100873135+thedilshan@users.noreply.github.com>
2022-05-17 18:50:12 +0530

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/divinedines/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/divinedines/ExampleInstrumentedTest.java
@@ -16,7 +16,8 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class ExampleInstrumentedTest 
+  {
     @Test
     public void useAppContext() {
         // Context of the app under test.
